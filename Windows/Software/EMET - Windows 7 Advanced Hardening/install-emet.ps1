9c4e466111e4215562df19361b753081c4b2a054
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-11-19 01:33:34 -0600

    Init EMET

---
+++ Windows/Software/EMET - Windows 7 Advanced Hardening/install-emet.ps1
@@ -0,0 +1,24 @@
+######SCRIPT FOR FULL INSTALL AND CONFIGURE ON STANDALONE MACHINE#####
+#Continue on error
+$ErrorActionPreference= 'silentlycontinue'
+
+#Require elivation for script run
+#Requires -RunAsAdministrator
+Write-Output "Elevating priviledges for this process"
+do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
+
+#Install EMET
+msiexec /i "EMET Setup.msi" /qn /norestart
+
+#Copy EMET Conf File
+Copy-Item EMET_Conf.xml "C:\Program Files (x86)\EMET 5.5"
+
+#Conf EMET Extras
+CD "C:\Program Files (x86)\EMET 5.5"
+.\EMET_Conf.exe --import --force ./EMET_Conf.xml
+.\EMET_Conf.exe --deephooks enabled
+.\EMET_Conf.exe --antidetours enabled
+.\EMET_Conf.exe --eafplus enabled
+.\EMET_Conf.exe --exploitaction stop
+.\EMET_Conf.exe --reporting -telemetry +eventlog +trayicon
+.\EMET_Conf.exe --list_system 
