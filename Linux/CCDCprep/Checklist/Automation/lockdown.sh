b6148e038b1d44b62c48730050cdcacd038692cd
Russell Brinson <russell@Russells-MacBook-Air.local>
2021-11-30 08:05:10 -0500

    Updated formatting, corrected } bracket that was missing at end of function, removed extra $OS I added

--- Linux/CCDCprep/Checklist/Automation/lockdown.sh
+++ Linux/CCDCprep/Checklist/Automation/lockdown.sh
@@ -6,61 +6,59 @@ for user in $(cut -f1 -d: /etc/passwd); do echo "###### $user crontab is:"; cat 
 }
 
 enumerate_func (){
-#enumerate system
-date -u  >> sysinfo.txt
-uname -a >> sysinfo.txt
+	#enumerate system
+	date -u  >> sysinfo.txt
+	uname -a >> sysinfo.txt
 
-# Added in error testing, who knows what gets borked
-# per https://www.linux.org/docs/man5/os-release.html
-# /usr/lib/os-release should be the fallback
+	# Added in error testing, who knows what gets borked
+	# per https://www.linux.org/docs/man5/os-release.html
+	# /usr/lib/os-release should be the fallback
 
-if . /etc/os-release ; then
-        OS=$NAME
-else
-        . /usr/lib/os-release
-        OS=$NAME
-fi
+	if . /etc/os-release ; then
+	        OS=$NAME
+	else
+	        . /usr/lib/os-release
+	        OS=$NAME
+	fi
+	
+	echo "OS is $ID" >> sysinfo.txt
+	lscpu    >> sysinfo.txt
+	lsblk    >> sysinfo.txt
+	ip a     >> sysinfo.txt
+	sudo netstat -auntp >>sysinfo.txt
+	df       >> sysinfo.txt
+	ls -latr /var/acc >> sysinfo.txt
+	sudo ls -latr /var/log/* >> sysinfo.txt
+	sudo ls -la /etc/syslog >> sysinfo.txt
+	check_crontab_func
+	cat /etc/crontab >> sysinfo.txt
+	ls -la /etc/cron.* >> sysinfo.txt
+	sestatus >> sysinfo.txt
+	getenforce >> sysinfo.txt
+	sudo cat /root/.bash_history >> sysinfo.txt
+	cat ~/.bash_history >> sysinfo.txt
+	cat /etc/group >> sysinfo.txt
+	cat /etc/passwd >> sysinfo.txt
+	
+	# If Debian or Ubuntu (or Arch if we add support), then ufw is installed
+	# ufw = Uncomplicated Firewall - https://help.ubuntu.com/community/UFW
+	if [ "$OS" = "Ubuntu" ]; then
+		ufw-status=$(sudo ufw status)
+		echo "ufw $ufw-status" >> sysinfo.txt
 
-echo "OS is $ID" >> sysinfo.txt
-lscpu    >> sysinfo.txt
-lsblk    >> sysinfo.txt
-ip a     >> sysinfo.txt
-sudo netstat -auntp >>sysinfo.txt
-df       >> sysinfo.txt
-ls -latr /var/acc >> sysinfo.txt
-sudo ls -latr /var/log/* >> sysinfo.txt
-sudo ls -la /etc/syslog >> sysinfo.txt
-check_crontab_func
-cat /etc/crontab >> sysinfo.txt
-ls -la /etc/cron.* >> sysinfo.txt
-sestatus >> sysinfo.txt
-getenforce >> sysinfo.txt
-sudo cat /root/.bash_history >> sysinfo.txt
-cat ~/.bash_history >> sysinfo.txt
-cat /etc/group >> sysinfo.txt
-cat /etc/passwd >> sysinfo.txt
+	elif [ "$OS" = "Debian" ]; then
+		ufw-status=$(sudo ufw status)
+		echo "ufw $ufw-status" >> sysinfo.txt
 
-# If Debian or Ubuntu (or Arch if we add support), then ufw is installed
-# ufw = Uncomplicated Firewall - https://help.ubuntu.com/community/UFW
-if [ "$OS" = "Ubuntu" ]; then
-
-ufw-status=$(sudo ufw status)
-echo "ufw $ufw-status" >> sysinfo.txt
-
-elif [ "$OS" = "Debian" ]; then
-
-ufw-status=$(sudo ufw status)
-echo "ufw $ufw-status" >> sysinfo.txt
-
-fi
+	fi
 }
 
 backup_admin_func (){
-#addbackup Admins
-adduser --disabled-password --gecos "" nightowl || echo "User Exists"
-adduser --disabled-password --gecos "" nightowl2 || echo "User Exists"
-usermod -aG sudo nightowl
-usermod -aG sudo nightowl2
+	#addbackup Admins
+	adduser --disabled-password --gecos "" nightowl || echo "User Exists"
+	adduser --disabled-password --gecos "" nightowl2 || echo "User Exists"
+	usermod -aG sudo nightowl
+	usermod -aG sudo nightowl2
 }
 
 list_users_func (){
@@ -68,59 +66,53 @@ list_users_func (){
 }
 
 change_passwords_func (){
-for i in `cat user_list.txt`
-do
-PASS=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 31)
-#PASS=test
-echo "Changing password for $i" 
-echo "$i,$PASS" >>  userlist.txt
-echo -e "$PASS\n$PASS" | passwd $i
-done
+	for i in `cat user_list.txt`
+	do
+		PASS=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 31)
+		#PASS=test
+		echo "Changing password for $i" 
+		echo "$i,$PASS" >>  userlist.txt
+		echo -e "$PASS\n$PASS" | passwd $i
+	done
+}
 
 install_tools_func () {
 
-# Added in error testing, who knows what gets borked
-# per https://www.linux.org/docs/man5/os-release.html
-# /usr/lib/os-release should be the fallback
+	# Added in error testing, who knows what gets borked
+	# per https://www.linux.org/docs/man5/os-release.html
+	# /usr/lib/os-release should be the fallback
 
-if . /etc/os-release ; then
-        OS=$NAME
-        echo "$OS"
-else
-        . /usr/lib/os-release
-        OS=$NAME
-        echo "$OS"
+	if . /etc/os-release ; then
+	        OS=$NAME
+	else
+	        . /usr/lib/os-release
+	        OS=$NAME
+	fi
 
-fi
+	echo "$OS installing tools"
 
-echo "$OS installing tools"
+	if [ "$OS" = "Ubuntu" ]; then
+		apt -y install fail2ban tripwire clamav inotify-tools
 
-if [ "$OS" = "Ubuntu" ]; then
+	elif [ "$OS" = "Debian" ]; then
+		apt -y install fail2ban tripwire clamav inotify-tools
 
-apt -y install fail2ban tripwire clamav inotify-tools
+	elif [ "$OS" = "CentOS Linux" ];then
+		yum -y install fail2ban tripwire clamav inotify-tools
 
-elif [ "$OS" = "Debian" ]; then
+	else 
+	echo "Not Ubuntu, Debian or CentOS, install tools manually"
 
-apt -y install fail2ban tripwire clamav inotify-tools
-
-elif [ "$OS" = "CentOS Linux" ];then
-
-yum -y install fail2ban tripwire clamav inotify-tools
-
-else 
-echo "Not Ubuntu, Debian or CentOS, install tools manually"
-
-fi
+	fi
 }
 
 
-}
 main_func (){
-enumerate_func
-backup_admin_func
-list_users_func
-change_passwords_func
-install_tools_func
+	enumerate_func
+	backup_admin_func
+	list_users_func
+	change_passwords_func
+	install_tools_func
 }
 
 main_func
